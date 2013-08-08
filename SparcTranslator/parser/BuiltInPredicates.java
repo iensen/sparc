1bcba9ca5518a21b38c271eb288a8aeffcd9de8c
Evgenii Balai <evgenii.balai@ttu.edu>
2013-08-07 19:08:34 -0500

    Add exit

--- SparcTranslator/parser/BuiltInPredicates.java
+++ SparcTranslator/parser/BuiltInPredicates.java
@@ -5,6 +5,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BuiltInPredicates {
-   public static final String[] predicateNames = new String[] { "appl", "has_grounding","warning"};
+   public static final String[] predicateNames = new String[] { "appl", "has_grounding","warning","exit"};
    public static final Set<String> predicateNamesSet = new HashSet<String>(Arrays.asList(predicateNames));
 }
--- SparcTranslator/querying/QueryEngine.java
+++ SparcTranslator/querying/QueryEngine.java
@@ -33,10 +33,11 @@ public class QueryEngine {
 	private ExternalSolver solver;
 	private AnswerSetParser answerSetParser;
 	private HashSet<String> queryVars;
-    TypeChecker tc;
-	public QueryEngine(ArrayList<AnswerSet> answerSets,TypeChecker tc) {
+	TypeChecker tc;
+
+	public QueryEngine(ArrayList<AnswerSet> answerSets, TypeChecker tc) {
 		sc = new Scanner(System.in);
-		this.tc=tc;
+		this.tc = tc;
 		this.answerSets = answerSets;
 		try {
 			if (Settings.getSolver() == ASPSolver.DLV) {
@@ -57,16 +58,17 @@ public class QueryEngine {
 		while (true) {
 			try {
 				query = readQuery();
-				if (query == null) {
+				if (query == null || query.toString().equals("exit")
+						|| query.toString().equals("exit.")) {
 					break;
 				}
 				answerQuery(query);
 			} catch (ParseException ex) {
 				System.err.println(ex.getMessage());
+			} catch (TokenMgrError ex) {
+				System.err
+						.println("your query must have syntax p(t1,t2...,tn) (where the list of terms may be omitted)");
 			}
-			 catch(TokenMgrError ex) {
-				 System.err.println("your query must have syntax p(t1,t2...,tn) (where the list of terms may be omitted)");
-			 }
 
 		}
 	}
@@ -77,74 +79,76 @@ public class QueryEngine {
 		StringReader sr = new StringReader(query);
 		parser = new QueryParser(sr);
 		QASTatom atom = parser.parseQuery();
-	    
+
 		return atom;
 	}
 
 	private void answerQuery(QASTatom query) {
 		queryVars = query.fetchVariables();
 		query.evaluateAllArithmetics();
-		 //check the query:	
-		 try {
-			    tc.ignoreLineNumbers=true;
-				tc.checkAtom(new ASTatom(query));
-			} catch (parser.ParseException e) {
-				// TODO Auto-generated catch block
-				System.err.println(e.getMessage());
-				return ;
-			}
-		
+		// check the query:
+		try {
+			tc.ignoreLineNumbers = true;
+			tc.checkAtom(new ASTatom(query));
+		} catch (parser.ParseException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+			return;
+		}
+
 		if (query.isGround()) {
 
-			
-			 answerGroundQuery(query);
+			answerGroundQuery(query);
 		} else {
 			answerNonGroundQuery(query);
 		}
 	}
 
-	private  void answerNonGroundQuery(QASTatom query) {
+	private void answerNonGroundQuery(QASTatom query) {
 
-		AnswerSet theOnlyAnswerSet =getAnswerSetOfCorrespondingASPProgram(query);
-		boolean answerFound=false;
-		for (String atom:theOnlyAnswerSet.atoms) {
+		AnswerSet theOnlyAnswerSet = getAnswerSetOfCorrespondingASPProgram(query);
+		boolean answerFound = false;
+		for (String atom : theOnlyAnswerSet.atoms) {
 			if (atom.startsWith("true_in_all_models")) {
-				answerFound=true;
-				Pair<String,ArrayList<String>> recordContent=StringListUtils.splitTerm(atom);
+				answerFound = true;
+				Pair<String, ArrayList<String>> recordContent = StringListUtils
+						.splitTerm(atom);
 				System.out.print(buildAnswer(recordContent.second));
-			    
-				String response=sc.nextLine();
-				while(!response.equals("") && !response.toLowerCase().equals("q")) {
-					System.err.print("Press Enter to continue or input \'q\' to interrupt the query");
-					response=sc.nextLine();
+
+				String response = sc.nextLine();
+				while (!response.equals("")
+						&& !response.toLowerCase().equals("q")) {
+					System.err
+							.print("Press Enter to continue or input \'q\' to interrupt the query");
+					response = sc.nextLine();
 				}
-				if(response.toLowerCase().equals("q")) {
+				if (response.toLowerCase().equals("q")) {
 					break;
 				}
 			}
 		}
-		if(!answerFound) {
+		if (!answerFound) {
 			System.out.println("unknown");
 		}
 
 	}
 
 	private String buildAnswer(ArrayList<String> terms) {
-		StringBuilder answer=new StringBuilder();
-		int index=0;
-		for(String var:queryVars) {
-            if(index!=0)
-            	answer.append(" ");
-			answer.append(var+" = "+terms.get(index));
-            ++index;
+		StringBuilder answer = new StringBuilder();
+		int index = 0;
+		for (String var : queryVars) {
+			if (index != 0)
+				answer.append(" ");
+			answer.append(var + " = " + terms.get(index));
+			++index;
 		}
 		return answer.toString();
 	}
 
 	private void answerGroundQuery(QASTatom query) {
-		
-		AnswerSet theOnlyAnswerSet =getAnswerSetOfCorrespondingASPProgram(query);
-	     // if there is an atom "true_in_all_models, it is yes"
+
+		AnswerSet theOnlyAnswerSet = getAnswerSetOfCorrespondingASPProgram(query);
+		// if there is an atom "true_in_all_models, it is yes"
 		// if there is an atom "false_in_all_models, it is no"
 		for (String atom : theOnlyAnswerSet.atoms) {
 			if (atom.startsWith("true_in_all_models")) {
@@ -171,10 +175,10 @@ public class QueryEngine {
 		return theOnlyAnswerSet;
 
 	}
-	
+
 	private String constructASPProgram(QASTatom query) {
 		int answerSetIndex = 1;
-	
+
 		StringBuilder prefix = new StringBuilder();
 		for (AnswerSet aSet : answerSets) {
 			for (String atom : aSet.atoms) {
@@ -221,14 +225,15 @@ public class QueryEngine {
 				+ ((queryVars.size() > 0) ? "("
 						+ StringListUtils.getSeparatedList(queryVars, ",")
 						+ ")" : "") + ":-" + bodyForFalseInAllModels + ".");
-       
+
 		return prefix.toString();
 
 	}
+
 	boolean isNumber(String s) {
 		Pattern isInteger = Pattern.compile("[1-9]\\d*");
 		return isInteger.matcher(s).matches();
-	
+
 	}
-	
+
 }

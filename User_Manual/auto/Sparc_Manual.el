cf429110703b413ec2baf9a0b96073c98dc21efb
Evgenii Balai <evgenii.balai@gmail.com>
2015-09-18 14:59:58 -0500

    Fix typos in the manual

--- User_Manual/auto/Sparc_Manual.el
+++ User_Manual/auto/Sparc_Manual.el
@@ -1,68 +1,67 @@
-(TeX-add-style-hook "Sparc_Manual"
+(TeX-add-style-hook
+ "Sparc_Manual"
  (lambda ()
-    (LaTeX-add-bibliographies
-     "mybib")
-    (LaTeX-add-environments
-     "definition"
-     "collorary"
-     "proposition"
-     "invariant"
-     "property"
-     "claim"
-     "example")
-    (LaTeX-add-labels
-     "marker"
-     "fig:dlv_solver_check"
-     "fig:clingo_solver_check"
-     "option"
-     "ss"
-     "type_warnings"
-     "asp_type_warnings"
-     "clp_type_warnings"
-     "fig:plug_install"
-     "fig:createfile"
-     "fig:openqueries"
-     "fig:runquery"
-     "fig:ansshow"
-     "fig:ansset"
-     "fig:ansres"
-     "fig:addswipl"
-     "fig:addprop")
-    (TeX-add-symbols
-     '("blk" 2)
-     '("ee" 1)
-     '("ie" 1)
-     '("ft" 1)
-     '("fe" 1)
-     '("pg" 1)
-     '("set" 1)
-     '("otherquestions" 1)
-     '("future" 1)
-     '("exercise" 1)
-     '("hide" 1)
-     "emptyclause"
-     "lplus"
-     "eoa"
-     "st")
-    (TeX-run-style-hooks
-     "listings"
-     "multicol"
-     "palatino"
-     "hyperref"
-     "breaklinks"
-     "footnote"
-     "fontenc"
-     "ulem"
-     "normalem"
-     "amsmath"
-     "graphicx"
-     "amssymb"
-     ""
-     "url"
-     "hyphens"
-     "latex2e"
-     "art12"
-     "article"
-     "letterpaper"
-     "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("url" "hyphens") ("ulem" "normalem") ("hyperref" "breaklinks")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "url"
+    "amssymb"
+    "graphicx"
+    "amsmath"
+    "ulem"
+    "fontenc"
+    "footnote"
+    "hyperref"
+    "palatino"
+    "multicol"
+    "listings")
+   (TeX-add-symbols
+    '("blk" 2)
+    '("ee" 1)
+    '("ie" 1)
+    '("ft" 1)
+    '("fe" 1)
+    '("pg" 1)
+    '("set" 1)
+    '("otherquestions" 1)
+    '("future" 1)
+    '("exercise" 1)
+    '("hide" 1)
+    "emptyclause"
+    "lplus"
+    "eoa"
+    "st")
+   (LaTeX-add-labels
+    "marker"
+    "fig:dlv_solver_check"
+    "fig:clingo_solver_check"
+    "option"
+    "ss"
+    "type_warnings"
+    "asp_type_warnings"
+    "clp_type_warnings"
+    "fig:plug_install"
+    "fig:createfile"
+    "fig:openqueries"
+    "fig:runquery"
+    "fig:ansshow"
+    "fig:ansset"
+    "fig:ansres"
+    "fig:addswipl"
+    "fig:addprop")
+   (LaTeX-add-environments
+    "definition"
+    "collorary"
+    "proposition"
+    "invariant"
+    "property"
+    "claim"
+    "example")
+   (LaTeX-add-bibliographies
+    "mybib")))
 
